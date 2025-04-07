5d52b4bc6c757d7b71bfb1030b18bd4f0829d698
Mitra Fazel <mitra.fazel@hotmail.com>
2025-04-07 17:57:38 +0100

    Project setup complete

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
 from setuptools import setup, find_packages
 
 # Read the requirements from the requirements.txt file
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
 setup(
-    name='etl_example',
-    version='0.1.0',
-    description=(
-        'An ETL project for extracting, transforming, and loading data.'
-    ),
-    author='Your Name',
-    author_email='ewright@digitalfutures.com',
-    url='https://github.com/digital-futures-academy-de-curriculum/etl-example',
+    name="etl_example",
+    version="0.1.0",
+    description=("An ETL project for extracting, transforming, and loading data."),
+    author="Your Name",
+    author_email="ewright@digitalfutures.com",
+    url="https://github.com/de-2502-a/etl-project-demo",
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
     entry_points={
-        'console_scripts': [
-            'run_etl=scripts.run_etl:main',
-            'run_tests=tests.run_tests:main',
+        "console_scripts": [
+            "run_etl=scripts.run_etl:main",
+            "run_tests=tests.run_tests:main",
         ],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
 
 
