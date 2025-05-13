d7f3c7972e023b6bec8320673853bcefe3203577
Mitra Fazel <mitra.fazel@hotmail.com>
2025-05-13 22:30:36 +0100

    Fixed dashboard filename, updated run_etl script

--- scripts/run_etl.py
+++ scripts/run_etl.py
@@ -3,10 +3,7 @@ from etl.transform.transform import transform_data
 from etl.load.load import load_to_csv
 
 def run_etl():
-<<<<<<< HEAD
-=======
     print("🔍 Running ETL pipeline...")
->>>>>>> eda
     df = extract_data("data/raw/SuperStoreOrders.csv")
     df = transform_data(df)
     load_to_csv(df, "data/cleaned/cleaned_superstore.csv")
