f7143fa3e99fd7dbdddeb2d1463d1e95ec7ff788
Mitra Fazel <mitra.fazel@hotmail.com>
2025-05-13 17:53:45 +0100

    merge

--- scripts/run_etl.py
+++ scripts/run_etl.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-from etl.transform.transform import transform_data
-from etl.load.load import load_to_csv
-from etl.extract.extract import extract_data
-
-def run_etl():
-    df = extract_data("/Users/mitra/Documents/digital-futures/capstone_project/superstore-streamlit-app/data/raw/SuperStoreOrders.csv")
-=======
 from etl.extract.extract import extract_data
 from etl.transform.transform import transform_data
 from etl.load.load import load_to_csv
 
 def run_etl():
     df = extract_data("data/raw/SuperStoreOrders.csv")
->>>>>>> etl-pipeline
     df = transform_data(df)
     load_to_csv(df, "data/cleaned/cleaned_superstore.csv")
     print("ETL process completed.")
